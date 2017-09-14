06986f1e4c0f1420119c6990338849ee0eda5780
atharvani <shital.sanap@gmail.com>
2017-09-14 15:42:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ function happyHolidays()
 {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(firstName)
+{
+  return `Happy holidays, ${firstName}!`
+}
+function happyHolidayTo(holiday,firstName)
+{
+  return `Happy ${holiday}, ${firstName}!`
+}
